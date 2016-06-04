27b76154ca2cd26f1ff7bf7400520fa49806e355
OA Wu <comdan66@gmail.com>
2016-06-04 22:31:40 +0800

    Fix compass config.

--- cmd/config.rb
+++ cmd/config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 
-http_path = "/oaf2e"
+http_path = "/browser_console_qrcode"
 
 fonts_dir = "font"
 fonts_path = "../font"
